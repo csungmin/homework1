8e2d82e4f45f3d5840746551114d8c3d73cd537b
csungmin <chuns045@newschool.edu>
2024-03-04 04:03:27 -0500

    homework4

--- homework4/script.js
+++ homework4/script.js
@@ -1,18 +1,23 @@
 // Add your javascript here
 
 document.addEventListener('DOMContentLoaded', function () {
-    const artworks = document.querySelectorAll('#click-me,#click-me-2');
-    const artworkInfos = document.querySelectorAll('.artwork-info, .artwork-info-2');
+  const artworks = document.querySelectorAll('#click-me, #click-me-2');
+  const artworkInfos = document.querySelectorAll('.artwork-info, .artwork-info-2');
 
-  artworks.forEach(function (artwork, index) {
-    artwork.addEventListener('click', function () {
-        const artworkInfo = artworkInfos[index];
+  artworks.forEach(function (button, index) {
+    button.addEventListener('click', function () {
+      const artwork = document.querySelectorAll('.artwork')[index];
+      const artworkInfo = artwork.querySelector('.artwork-info, .artwork-info-2');
+
+          console.log("artworks: ", artwork);
+          console.log("artworkInfo: ", artworkInfo);
+
+          if (artworkInfo.style.opacity == '0' || artworkInfo.style.opacity == '') {
+              artworkInfo.style.opacity = '1';
+          } else {
+              artworkInfo.style.opacity = '0';
+          }
       
-        if (artworkInfo.style.opacity == '0' || artworkInfo.style.opacity == '') {
-            artworkInfo.style.opacity = '1';
-        } else {
-            artworkInfo.style.opacity = '0';
-        }
-    });
+      });
   });
 });
